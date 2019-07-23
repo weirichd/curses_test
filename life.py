75001ca718996cd6248b707a86720112340e4247
David Edward Weirich <weirich.david@gmail.com>
2019-07-22 23:41:00 -0400

    Working demo for game of life

---
+++ life.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+import curses
+import time
+
+
+def update_life(X):
+    """
+    I admit I stole this from somewhere, but ONLY after 
+    taking the time to figure out how it works!
+    """
+    n_neighbors = sum(np.roll(np.roll(X, i, 0), j, 1)
+                     for i in (-1, 0, 1) for j in (-1, 0, 1)
+                     if (i != 0 or j != 0))
+    return (n_neighbors == 3) | (X & (n_neighbors == 2))
+
+
+def maze_run(stdscr):
+    stdscr.nodelay(True)
+
+    board = np.random.randint(0, 2, size=(10, 10))
+
+    go = True
+
+    while(go):
+        # Update
+        c = stdscr.getch()
+        if c == ord('q'):
+            go = False
+
+        board = update_life(maze)
+
+        # Render
+        stdscr.clear()
+
+        for i in np.ndindex(maze.shape):
+            stdscr.addstr(*i, "{}".format(' ' if maze[i] == 0 else '#'))
+            stdscr.refresh()
+
+        # Sleep
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    curses.wrapper(maze_run)
