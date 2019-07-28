464a7b55f419aadd22ec702ef982b77cfac5f511
David Edward Weirich <weirich.david@gmail.com>
2019-07-28 12:38:40 -0400

    rotation

--- tetris.py
+++ tetris.py
@@ -1,10 +1,9 @@
 import numpy as np
-
 import curses
 import time
 
-PLAY_WIDTH = 20
-PLAY_HEIGHT = 35
+PLAY_WIDTH = 10
+PLAY_HEIGHT = 20
 O_COLOR = 1
 I_COLOR = 2
 T_COLOR = 3
@@ -15,14 +14,41 @@ L_COLOR = 7
 BORDER_COLOR = 8
 
 
+# Sprites
 tetrominoes = [
-    np.array([[0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0]]),
-    np.array([[0, 0, 2, 0], [0, 0, 2, 0], [0, 0, 2, 0], [0, 0, 2, 0]]),
-    np.array([[0, 0, 3, 0], [0, 3, 3, 3], [0, 0, 0, 0], [0, 0, 0, 0]]),
-    np.array([[0, 4, 4, 0], [0, 0, 4, 4], [0, 0, 0, 0], [0, 0, 0, 0]]),
-    np.array([[0, 0, 5, 5], [0, 5, 5, 0], [0, 0, 0, 0], [0, 0, 0, 0]]),
-    np.array([[0, 6, 0, 0], [0, 6, 6, 6], [0, 0, 0, 0], [0, 0, 0, 0]]),
-    np.array([[0, 0, 0, 7], [0, 7, 7, 7], [0, 0, 0, 0], [0, 0, 0, 0]]),
+    [  # 0
+        np.array([[1, 1], [1, 1]])
+    ],
+    [  # I
+        np.array([[0, 0, 2, 0], [0, 0, 2, 0], [0, 0, 2, 0], [0, 0, 2, 0]]),
+        np.array([[0, 0, 0, 0], [2, 2, 2, 2], [0, 0, 0, 0], [0, 0, 0, 0]]),
+    ],
+    [  # T
+        np.array([[0, 3, 0], [3, 3, 3], [0, 0, 0]]),
+        np.array([[0, 3, 0], [0, 3, 3], [0, 3, 0]]),
+        np.array([[0, 0, 0], [3, 3, 3], [0, 3, 0]]),
+        np.array([[0, 3, 0], [3, 3, 0], [0, 3, 0]]),
+    ],
+    [  # Z
+        np.array([[4, 4, 0], [0, 4, 4], [0, 0, 0]]),
+        np.array([[0, 0, 4], [0, 4, 4], [0, 4, 0]]),
+    ],
+    [  # S
+        np.array([[0, 5, 5], [5, 5, 0], [0, 0, 0]]),
+        np.array([[0, 5, 0], [0, 5, 5], [0, 0, 5]]),
+    ],
+    [  # J
+        np.array([[6, 0, 0], [6, 6, 6], [0, 0, 0]]),
+        np.array([[0, 6, 6], [0, 6, 0], [0, 6, 0]]),
+        np.array([[0, 0, 0], [6, 6, 6], [0, 0, 6]]),
+        np.array([[0, 6, 0], [0, 6, 0], [6, 6, 0]]),
+    ],
+    [  # L
+        np.array([[0, 0, 7], [7, 7, 7], [0, 0, 0]]),
+        np.array([[0, 7, 0], [0, 7, 0], [0, 7, 7]]),
+        np.array([[0, 0, 0], [7, 7, 7], [7, 0, 0]]),
+        np.array([[7, 7, 0], [0, 7, 0], [0, 7, 0]]),
+    ]
 ]
 
 
@@ -40,41 +66,76 @@ def init_colors():
 def draw_boarder(window):
     window.addstr(4, 10, "TETRIS - Press Q to quit")
 
-    window.addstr(6, 6, "+" + "-" * PLAY_WIDTH + "+", curses.color_pair(BORDER_COLOR))
-    window.addstr(7 + PLAY_HEIGHT, 6, "+" + "-" * PLAY_WIDTH + "+", curses.color_pair(BORDER_COLOR))
+    window.addstr(6, 6, "\u250f" + "\u2501" * PLAY_WIDTH + "\u2513", curses.color_pair(BORDER_COLOR))
+    window.addstr(7 + PLAY_HEIGHT, 6, "\u2517" + "\u2501" * PLAY_WIDTH + "\u251b", curses.color_pair(BORDER_COLOR))
     for i in range(7, 7 + PLAY_HEIGHT):
-        window.addstr(i, 6, "|", curses.color_pair(BORDER_COLOR))
-        window.addstr(i, 6 + PLAY_WIDTH + 1, "|", curses.color_pair(BORDER_COLOR))
+        window.addstr(i, 6, "\u2503", curses.color_pair(BORDER_COLOR))
+        window.addstr(i, 6 + PLAY_WIDTH + 1, "\u2503", curses.color_pair(BORDER_COLOR))
 
 
-def draw_tetras(window):
-    for j in range(7):
-        for i in np.ndindex(tetrominoes[1].shape):
-            window.addstr(i[1] + 10 + j*4, i[0] + 10, " ", curses.color_pair(tetrominoes[j][i]))
+def draw_tetra(window, piece, position_x, position_y, rotation):
+    rotation = rotation % len(tetrominoes[piece])
+
+    for i in np.ndindex(tetrominoes[piece][rotation].shape):
+        p = tetrominoes[piece][rotation][i]
+        if p != 0:
+            window.addstr(
+                i[1] + position_y + 7,
+                i[0] + position_x + 7,
+                "\u2591",
+                curses.color_pair(p)
+            )
 
 
 def board_run(stdscr):
     stdscr.nodelay(True)
     init_colors()
+    curses.curs_set(0)
+
+    # Initialize Game
+    player_x, player_y = 0, 0
+    rotation = 0
+    current_piece = np.random.randint(7)
+    frames_per_step = 10
+    current_frame_number = 0
 
     go = True
 
-    while(go):
+    while go:
         # Update
+
+        # Player Input
         c = stdscr.getch()
         if c == ord('q'):
             go = False
+        if c == curses.KEY_LEFT:
+            player_x = max(0, player_x - 1)  # TODO:  Actual Collision
+        if c == curses.KEY_RIGHT:
+            player_x = min(PLAY_WIDTH - 4, player_x + 1)  # TODO:  Actual Collision
+        if c == curses.KEY_UP:
+            rotation = rotation + 1
+
+        # Advance Game State
+        current_frame_number = current_frame_number + 1
+        if current_frame_number == frames_per_step:
+            current_frame_number = 0
+            player_y = player_y + 1
+
+        if player_y == PLAY_HEIGHT - 4:   # TODO:  Actual Collision
+            player_x, player_y = 0, 0
+            current_piece = np.random.randint(7)
+            rotation = 0
 
         # Render
         stdscr.clear()
 
         draw_boarder(stdscr)
-        draw_tetras(stdscr)
+        draw_tetra(stdscr, current_piece, player_x, player_y, rotation)
 
         stdscr.refresh()
 
         # Sleep
-        time.sleep(.1)
+        time.sleep(.03)
 
 
 if __name__ == '__main__':
